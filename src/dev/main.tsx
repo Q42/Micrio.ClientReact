74450fb0d7ccd7c32f8e537bfddde263205c59ed
Jiri Swen <jiri.ei@hotmail.com>
2025-04-24 15:17:24 +0200

    Improve dev setup testing attribute set/unset behaviour

--- src/dev/main.tsx
+++ src/dev/main.tsx
@@ -1,15 +1,25 @@
-// filepath: src/test-setup/main.tsx
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Micrio from '..';
 
-const App = () => (
-  <Micrio
-    id="dzzLm"
-    style={{ margin: 0, padding: 0, width: '100%', height: '100vh', backgroundColor: 'black' }}
-    onZoom={(event) => console.log('Zoom event triggered:', event)}
-  />
-);
+const App = () => {
+  const [turnOnPanStartAndSocial, setTurnOnPanStartAndSocial] = React.useState(false);
 
-// If using React 18+, you can use createRoot instead
+  return (
+    <div>
+      <Micrio
+        id="dzzLm"
+        style={{ margin: 0, padding: 0, width: '100%', height: '100vh', backgroundColor: 'black' }}
+        onZoom={e => console.log('Zoom event triggered:', e)}
+        dataSocial={turnOnPanStartAndSocial ? true : false}
+        onPanstart={turnOnPanStartAndSocial ? e => console.log('Panstart event triggered:', e) : undefined}
+        dataLogo={false}
+      />
+      <button onClick={() => setTurnOnPanStartAndSocial(!turnOnPanStartAndSocial)} style={{ position: 'absolute', bottom: 72, left: 24 }}>
+        {turnOnPanStartAndSocial ? 'Turn Off Panstart and Logo' : 'Turn On Panstart and Logo'}
+      </button>
+    </div>
+  );
+};
+
 createRoot(document.getElementById('root')!).render(<App />);
